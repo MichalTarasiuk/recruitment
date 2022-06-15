55e418efceafab3a750b962dc90a93d78d90fc1b
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-06-15 20:20:14 +0200

    fix server rendering in home page

--- task_1/src/modules/home/home.page.tsx
+++ task_1/src/modules/home/home.page.tsx
@@ -25,22 +25,18 @@ export const getServerSideProps = async ({
   query,
 }: GetServerSidePropsContext) => {
   try {
-    if (isString(query.page)) {
-      const { characters, previous, next } = await fetchCharacters(query.page)
+    const pageId = isString(query.page) ? query.page : '1'
+    const { characters, previous, next } = await fetchCharacters(pageId)
 
-      const paginationQueries = mapObject({ previous, next }, (key, value) =>
-        value ? [key, getSearchParam(value, 'page')] : [key, value]
-      )
+    const paginationQueries = mapObject({ previous, next }, (key, value) =>
+      value ? [key, getSearchParam(value, 'page')] : [key, value]
+    )
 
-      return {
-        props: {
-          characters,
-          paginationQueries,
-        },
-      }
-    } else {
-      console.log('err')
-      throw Error('Invalid page')
+    return {
+      props: {
+        characters,
+        paginationQueries,
+      },
     }
   } catch {
     return { notFound: true }
