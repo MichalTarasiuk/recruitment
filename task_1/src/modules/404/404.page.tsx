88ff7423a2e299cb5d47db6bb3ed23fc12a5e5a6
Tarasiukowski <tarasiuk.michal03@gmail.com>
2022-06-15 12:16:53 +0200

    create 404 page

---
+++ task_1/src/modules/404/404.page.tsx
@@ -0,0 +1,5 @@
+const NOT_FOUND_CODE = 404
+
+export const NotFoundPage = () => {
+  return <h1>NOT FOUND - {NOT_FOUND_CODE}</h1>
+}
---
+++ task_1/src/pages/404.tsx
@@ -0,0 +1,3 @@
+import { NotFoundPage } from 'src/modules/404/404.page'
+
+export default NotFoundPage
